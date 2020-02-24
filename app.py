f96c7991e27ada7677422446df3534f837203d81
Kimberly Souravong <kimberly-souravong@uiowa.edu>
2020-02-24 01:33:04 -0600

    Created loan formula

--- app.py
+++ app.py
@@ -21,6 +21,15 @@ def index():
         print(discount)
         print(totalPay)
 
+        A = loan
+        n = numPay * 12
+        i = intRate / 12
+        D = (((1 + i) ^n ) - 1 ) / ( i (1 + i) ^n)
+        P = A/D 
+
+        print(P)
+
+
     return render_template('index.html', pageTitle='Loan Calculator')
     
 
